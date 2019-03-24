3d844ea1f35d8c0aded18476caad50e38081df6a
Lior Nevo <lior.nevo@protegrity.com>
2019-03-24 11:23:49 -0400

    added orbitalcontrols

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var CannonDebugRenderer = require("./CannonDebugRenderer").CannonDebugRenderer;
 var Joystick = require('./Joystick').JoyStick;
 var GLBLoader = require('./GLTFLoader').GLBLoader;
 var FBXLoader = require('./FBXLoader').FBXLoader;
+var OrbitalControls = require('./OrbitalControls').OrbitalControls;
 
 module.exports = {
   Joystick: Joystick,
   CannonDebugRenderer: CannonDebugRenderer,
   GLBLoader: GLBLoader,
   FBXLoader: FBXLoader,
-  CannonHelper: CannonHelper
+  CannonHelper: CannonHelper,
+  OrbitalControls: OrbitalControls
 };
